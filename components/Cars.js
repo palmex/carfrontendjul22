219bce1b34a8b0b77740500d42ccf22bc20f253a
Admin Server <apostlma@ucsd.edu>
2022-07-14 13:47:37 +0000

    added states & text input w/ one state change

--- components/Cars.js
+++ components/Cars.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 
 export default class Cars extends React.Component {
 
     constructor(){
         super(); 
-        this.state = {stateBool: true}
-        this.state = {stateText: "ON"}
+        this.state = {
+            stateBool: true,
+            make: 'Jeep', 
+            model: 'Compass', 
+            year: '2019',
+            odometer: '56043'
+        
+        
+        }
+       
+
+        
     }
 
 
@@ -19,15 +29,41 @@ export default class Cars extends React.Component {
         } else {
             this.setState({stateText: "OFF"})
         }
+        
     }
 
 
     render(){
+
         return(
             <View style={styles.container}>
+                <Text>Make </Text>
+                <TextInput style={styles.textinput}
+                defaultValue={this.state.make}
+                onChangeText={(e) => this.setState({make: e})}></TextInput>
+
+
+                <Text>Model </Text>
+                <TextInput style={styles.textinput}
+                defaultValue={this.state.model}></TextInput>
+
+
+                <Text>Year </Text>
+                <TextInput style={styles.textinput}
+                defaultValue={this.state.year}></TextInput>
+
+
+                <Text>Odometer </Text>
+                <TextInput style={styles.textinput}
+                defaultValue={this.state.odometer}></TextInput>
+
+
                 <Button onPress={this.firstFunction} title="Button"/>
-                <Text>{this.state.stateText}</Text>
-                {/* <StatusBar style="auto" /> */}
+                <Text >{this.state.make}</Text>
+                <Text >{this.state.model}</Text>
+                <Text >{this.state.year}</Text>
+                <Text >{this.state.odometer}</Text>
+                
             </View>
 
         )
@@ -37,8 +73,15 @@ export default class Cars extends React.Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+    //   backgroundColor: {this.state.bkgColor},
       alignItems: 'center',
       justifyContent: 'center',
     },
+    textinput: {
+        height: 26, 
+        borderWidth: 0.5, 
+        borderColor: '#222', 
+        padding: 4, 
+        marginBottom: '1rem'
+    }
   });
